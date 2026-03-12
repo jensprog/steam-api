0e732645dbd82ba104d4392b7ba024baab75d8bb
Jens <jk224vx@student.lnu.se>
2026-03-12 09:39:58 +0100

    Added json streaming to the extractor since the proccess was killed on the server, trying to resolve the issue.

--- src/extractor.py
+++ src/extractor.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import pandas as pd
-import json
+import ijson
 
 
 def extract_games_json(json_path: str, max_games: int | None = None) -> pd.DataFrame:
@@ -9,20 +9,22 @@ def extract_games_json(json_path: str, max_games: int | None = None) -> pd.DataF
     if not path.exists():
         raise FileNotFoundError(f"JSON file not found: {path}")
     print(f"Extracting data from {path}...")
-    with open(path, "r", encoding="utf-8") as file:
-        data = json.load(file)
 
-        games_list = []
-        count = 0
-        for app_id, game_data in data.items():
+    games_list = []
+    count = 0
+
+    with open(path, "rb") as file:
+        parser = ijson.kvitems(file, "", use_float=True)
+
+        for app_id, game_data in parser:
             if max_games and count >= max_games:
                 break
-            game_data["app_id"] = app_id
-            games_list.append(game_data)
-            count += 1
+
+            if isinstance(game_data, dict):
+                game_data["app_id"] = app_id
+                games_list.append(game_data)
+                count += 1
 
     df = pd.DataFrame(games_list)
-    if max_games is not None:
-        df = df.head(max_games)
     print(f"Extracted {len(df)} rows.")
     return df
