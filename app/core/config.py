17cd6a6f951ef43c58d4c8d7a9faffacf39b13c2
Jens <jk224vx@student.lnu.se>
2026-04-07 10:23:06 +0200

    Updated google redirect uri to redirect to the correct frontend endpoint.

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     GOOGLE_CLIENT_ID: str = ""
     GOOGLE_CLIENT_SECRET: str = ""
-    GOOGLE_REDIRECT_URI: str = "http://localhost:8000/auth/google/callback"
+    GOOGLE_REDIRECT_URI: str = "http://localhost:3000/api/auth/callback"
 
     class Config:
         env_file = ".env"
