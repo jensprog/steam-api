2ee587ecd45c0b3471e40ea51ffad9a519cda781
Jens <jk224vx@student.lnu.se>
2026-04-11 12:29:02 +0200

    Implemented def to get games with their amount of players in a dict.

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.services.stats_service import get_games_by_price
+from app.services.stats_service import get_games_by_amount_of_players, get_games_by_price
 
 router = APIRouter(tags=["Stats"])
 
@@ -9,3 +9,8 @@ router = APIRouter(tags=["Stats"])
 @router.get("/games/by-price", status_code=status.HTTP_200_OK)
 def get_games_and_price(db: Session = Depends(get_db)):
     return get_games_by_price(db)
+
+
+@router.get("/games/by-owners", status_code=status.HTTP_200_OK)
+def get_games_by_player_amount(db: Session = Depends(get_db)):
+    return get_games_by_amount_of_players(db)
--- app/services/stats_service.py
+++ app/services/stats_service.py
@@ -16,3 +16,29 @@ def get_games_by_price(db: Session):
         else:
             price_sort_dict["Over $30"] += 1
     return [{"name": n, "value": v} for n, v in price_sort_dict.items()]
+
+
+def get_games_by_amount_of_players(db: Session):
+    games_with_players = (
+        db.query(Game.name, Game.estimated_owners).filter(Game.estimated_owners != None).all()  # noqa: E711
+    )
+    sort_estimated_players = {"No Owners": 0, "Under 50k": 0, "50k-200k": 0, "200k-1M": 0, "Over 1M": 0}
+
+    for estimated_owners in games_with_players:
+        lower = int(
+            estimated_owners.estimated_owners.split(
+                " - ",
+            )[0]
+        )
+
+        if lower == 0:
+            sort_estimated_players["No Owners"] += 1
+        elif lower < 50000:
+            sort_estimated_players["Under 50k"] += 1
+        elif lower < 200000:
+            sort_estimated_players["50k-200k"] += 1
+        elif lower < 1000000:
+            sort_estimated_players["200k-1M"] += 1
+        else:
+            sort_estimated_players["Over 1M"] += 1
+    return [{"name": n, "value": v} for n, v in sort_estimated_players.items()]
